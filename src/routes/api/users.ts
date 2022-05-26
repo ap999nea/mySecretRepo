6dad8a57032a46d37d2c4b90121cbe520bef553f
Giulia Della Vecchia <giuliadellavecchia96@gmail.com>
2022-05-26 19:15:48 +0200

    first paolo

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -1,13 +1,35 @@
 import { Router } from "express";
 import * as UserController from "../../controllers/users.controller";
-const userRouter= Router();
+const userRouter = Router();
+import { sign, verify } from "jsonwebtoken";
 
-userRouter.get('/', UserController.list)
+userRouter.get(
+  "/private",
+  (req, res, next) => {
+    // auth mi restituisce Bearer <token>
+    const auth = req.headers["authorization"];
+    //@ts-ignore
+    const token = /Bearer (.+)/.exec(auth)[1];
+    console.log(token);
+    //@ts-ignore
+    const isAuthorized = verify(token, process.env.SECRET);
+    if (isAuthorized) {
+      next();
+    } else {
+      return res.status(401).json({ msg: "Unauthorized" });
+    }
+  },
+  (req, res) => res.json({ msg: "Hello, it's me" })
+);
 
-userRouter.post('/', UserController.store)
+userRouter.get("/", UserController.list);
 
-userRouter.get('/:username', UserController.show)
+userRouter.post("/", UserController.store);
 
-userRouter.get('/:username/todos', UserController.userTodo)
+userRouter.get("/:username", UserController.show);
+
+userRouter.get("/:username/todos", UserController.userTodo);
+
+userRouter.post("/login", UserController.login);
 
 export default userRouter;
